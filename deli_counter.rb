d2fd0c283574b5d19130e716a48d3b00c619a381
Chris Kakos <ckakos@gmail.com>
2018-12-08 19:56:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def line(line_order)
+    position = []
+    if line_order == 0
+        return "There is nobody in line."
+    else
+        line_order.each_with_index do |value, index|
+            return "The line is currently #{index.to_i+1}. #{value}"
+    end
+end
